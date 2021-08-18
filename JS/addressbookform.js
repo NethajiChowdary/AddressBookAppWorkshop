f6cfa6f6b23889ee7450fca8c885b8937f9f67c3
NethajiChowdary <nethajichowdary555@gmail.com>
2021-08-18 07:04:37 +0530

    Added: Code to Capture the user Entry Using Getters and Setters

--- JS/addressbookform.js
+++ JS/addressbookform.js
@@ -1,64 +1,49 @@
-window.addEventListener('DOMContentLoaded', (event) => 
-{
+window.addEventListener('DOMContentLoaded', (event) => {
 
     const name = document.querySelector('#name'); 
     const textError = document.querySelector('.name-error');
     name.addEventListener('input', function () {
-      let names = document.querySelector('#name').value.split(" ");
-      if (names[0].length == 0) {
+      let name = document.querySelector('#name').value;
+      
+    try{
+        (new Contact()).firstName = name;
         textError.textContent = "";
-        return;
-      }
-      if (names.length == 2) {
-          let nameRegex = RegExp('^[A-Z][a-z]{2,}$')
-          if(!nameRegex.test(names[0]))
-          textError.textContent = 'First Name Invalid';
-          if(!nameRegex.test(names[1]))
-          textError.textContent = 'Last Name Invalid';
-          if(nameRegex.test(names[0])&&nameRegex.test(names[1]))
-          textError.textContent = "";
-      }
-      else {
-        let nameRegex = RegExp('^[A-Z][a-z]{2,}$');
-        if(!nameRegex.test(names[0]))
-        textError.textContent = 'First Name Invalid';
-        else
-        textError.textContent = "";
-      }
+    }catch(e){
+        textError.textContent = e;
+    }
     });
   
     const addressElement = document.querySelector('#address');
     const addressError = document.querySelector('.address-error');
     addressElement.addEventListener('input', function() {
       let address = document.querySelector('#address').value;
-      let addressRegex = RegExp("^[a-zA-Z0-9]{3,}\\s[a-zA-Z0-9]{3,}([ ]|[a-zA-Z0-9]{3,})*?$");
-      if(addressRegex.test(address)){
+      try{
+        (new Contact()).address = address;
         addressError.textContent = ""
+      }catch(e){
+        addressError.textContent = e;
       }
-        else{
-          addressError.textContent = "Invalid";
-        }
-      
     });
   
     const phoneElement = document.querySelector('#phone');
     const phoneError = document.querySelector('.phone-error');
     phoneElement.addEventListener('input', function() {
       let phone = document.querySelector('#phone').value;
-      let phoneRegex1 = RegExp("^[0-9]{2}[ ][0-9]{10}$");
-      let phoneRegex2 = RegExp("^[0-9]{2}[1-9][0-9]{9}$");
-      let phoneRegex3 = RegExp("^[+][0-9]{2}[1-9][0-9]{9}$");
-      if(phoneRegex1.test(phone) || phoneRegex2.test(phone) || phoneRegex3.test(phone))
-      phoneError.textContent = "";
-    
-      else{
-      phoneError.textContent = "Phone Number Invalid";
+      try{
+        (new Contact()).phone = phone;
+        phoneError.textContent = "";
+      }catch(e){
+        phoneError.textContent = e;
       }
   
     });
-});    
-const save = (event) => {
+  
+  });
+  
+  const save = (event) => {
+    console.log("sdf");
     event.preventDefault();
+    event.stopPropagation();
     let contactData = createContact();
     let jsonObject = JSON.stringify(contactData);
     alert(jsonObject);
